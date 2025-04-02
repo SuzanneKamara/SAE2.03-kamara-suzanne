46afc2d1fb81badd706732f8b5c62b4cf0a71dcc
Kamara Suzanne <kamara4@mmi-dev.unilim.fr>
2025-04-02 18:10:55 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "kamara4");
+define("DBLOGIN", "kamara4");
+define("DBPWD", "kamara4");
