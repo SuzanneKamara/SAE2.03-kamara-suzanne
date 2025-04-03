c8d862f3350814789601591f8c377b11f604c3e7
Kamara Suzanne <kamara4@mmi-dev.unilim.fr>
2025-04-03 16:25:52 +0200

    .

---
+++ admin/data/dataMovie.js
@@ -0,0 +1,70 @@
+
+// URL où se trouve le répertoire "server" sur mmi.unilim.fr
+let HOST_URL = "https://mmi.unilim.fr/~kamara4/SAE2.03-kamara-suzanne";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
+
+let DataMovie = {};
+
+
+ 
+
+
+/** DataMovie.update
+ * 
+ * Prend en paramètre un objet FormData (données de formulaire) à envoyer au serveur.
+ * Ces données sont incluses dans une requête HTTP en méthode POST.
+ * Une requête POST au lieu de GET n'affiche pas les données dans l'URL (plus discret).
+ * Les données sont placées dans le corps (body) de la requête HTTP. Elles restent visibles mais
+ * en utilisant les outils de développement du navigateur (Network > Payload).
+ * La requête comprend aussi un paramètre todo valant update pour indiquer au serveur qu'il
+ * s'agit d'une mise à jour (car on a codé le serveur pour qu'il sache quoi faire en fonction de la valeur de todo).
+ * 
+ * @param {*} fdata un objet FormData contenant les données du formulaire à envoyer au serveur.
+ * @returns la réponse du serveur.
+ */
+DataMovie.add = async function (fdata) {
+    // fetch possède un deuxième paramètre (optionnel) qui est un objet de configuration de la requête HTTP:
+    //  - method : la méthode HTTP à utiliser (GET, POST...)
+    //  - body : les données à envoyer au serveur (sous forme d'objet FormData ou bien d'une chaîne de caractères, par exempe JSON)
+    let config = {
+        method: "POST", // méthode HTTP à utiliser
+        body: fdata // données à envoyer sous forme d'objet FormData
+    };
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=add", config);
+    let data = await answer.json();
+    return data;
+}
+
+
+/** DataMovie.delete
+ * 
+ *  fait une requête HTTP au serveur pour demander la suppression d'un Movie
+ *  @param {*} fdata un objet FormData contenant les données du formulaire à envoyer au serveur (semaine et jour)
+ *  @returns la réponse du serveur.
+*/
+DataMovie.delete = async function (fdata) {
+    let config = {
+        method: "POST",
+        body: fdata
+    };
+   
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=delete", config);
+    let data = await answer.json();
+    return data;
+}
+
+export {DataMovie};
+
+
+
+
+/* Rappel : async / await ?
+    
+   Il y a des instructions qui prennent du temps sans qu'on puisse prédire combien.
+   fetch (et answer.json() ) en font partie.
+   Il n'est en effet pas possible de savoir combien de temps le serveur prendra à nous répondre.
+   Peut-être même qu'il est en panne et ne répondra pas du tout !
+   Le mot clé await permet de dire à javascript qu'il faut ATTENDRE la réponse du serveur avant de 
+   poursuivre l'exécution du code (sinon on va vouloir lire les données avant de les avoir reçues).
+   Et pour pouvoir utiliser await, il faut ajouter le mot clé async à la fonction.
+
+*/
